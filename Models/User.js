4dd6cc039781fb2d81817f26b2e55bf9160563fb
Jonathan-Omasajuwa <joemaniomas@gmail.com>
2026-07-30 17:34:19 +0100

    firstpush

---
+++ Models/User.js
@@ -0,0 +1,40 @@
+// Mongoose
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// User Schema
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other'],
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    required: true,
+    default: 'user'
+  },
+  timestamps: true // Date created and updated timestamps will be automatically added to the schema
+});
+
+// Create model from the schema
+const User = mongoose.model('User', UserSchema);
---
+++ Models/product.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+// Product Schema
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+  size: {
+    type: String,
+    enum: [],
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  timestamps: true // Date created and updated timestamps will be automatically added to the schema
+});
+
+// Create model from the schema
+const Product = mongoose.model('Product', ProductSchema);
